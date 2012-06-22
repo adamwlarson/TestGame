a341acac1d75866888f54e46f8b25ce8fef4a4a2
maddogawl <maddogawl@gmail.com>
2012-06-21 21:23:10 -0500

    Initial Test

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+// if you implement onInit, you must call cb() when done, so the framework knows when to continue start-up
+exports.onInit = function(feather, cb) {
+
+    if (typeof cb === "function") cb();
+};
+
+exports.onReady = function(feather) {
+
+};
+
+exports.onRequest = function(feather, req, res, next) {
+
+};
+*/
---
+++ public/widgets/game/game.client.js
@@ -0,0 +1,15 @@
+feather.ns("RiseTestGame");
+(function() {
+  RiseTestGame.game = feather.Widget.create({
+    name: "RiseTestGame.game",
+    path: "widgets/game/",
+    prototype: {
+      onInit: function() {
+        
+      },
+      onReady: function() {
+        
+      }
+    }
+  });
+})();
